62566c1de65029ba1704c2039f27add6a537bea8
ankitpriyarup <31309984+ankitpriyarup@users.noreply.github.com>
2023-06-12 01:51:10 +0530

    2 files modified

--- src/custom_picker.ts
+++ src/custom_picker.ts
@@ -4,26 +4,45 @@ import {
   Component,
   CUSTOM_ELEMENTS_SCHEMA,
 } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import '@polymer/paper-input/paper-textarea';
 
 @Component({
   selector: 'custom-picker',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-  <form class="review">
-    <h3 class="review-head">Rate us</h3>
+  <form [formGroup]="form" class="review" (ngSubmit)="onSubmit()">
+    <h2 class="review-head">Rate us</h2>
+    <!--
     <paper-textarea
+      ngDefaultControl
+      formControlName="reviewText"
       placeholder="Your honest review here..."
       label="Review text"
       rows="2"
       class="review-text"
     >
     </paper-textarea>
-    <button class="save">Save</button>
+    -->
+
+    <div class="review-textarea" contenteditable>
+      <h3 data-placeholder="Title..."></h3>
+      <p data-placeholder="Describe your rating"></p>
+    </div>
+    <button type="submit" class="save">Save</button>
   </form>
   `,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CustomPicker {}
+export class CustomPicker {
+  form = this.fb.group({ reviewText: '' });
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.form.reset();
+  }
+}
