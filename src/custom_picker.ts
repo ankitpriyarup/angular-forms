306f269d1d9e72a72453c8f20556d93d6a8a503f
ankitpriyarup <31309984+ankitpriyarup@users.noreply.github.com>
2023-06-12 01:22:02 +0530

    3 files modified

--- src/custom_picker.ts
+++ src/custom_picker.ts
@@ -1,11 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
+import '@polymer/paper-input/paper-textarea';
 
 @Component({
   selector: 'custom-picker',
   standalone: true,
   imports: [CommonModule],
-  template: `It works!`,
+  template: `
+  <form class="review">
+    <h3 class="review-head">Rate us</h3>
+    <paper-textarea
+      placeholder="Your honest review here..."
+      label="Review text"
+      rows="2"
+      class="review-text"
+    >
+    </paper-textarea>
+    <button class="save">Save</button>
+  </form>
+  `,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomPicker {}
